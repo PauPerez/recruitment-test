72196d64e78c642808bac3c3374988afd8e50efa
PauPerez <pau_perezsolvez@iescarlesvallbona.cat>
2022-06-17 16:08:51 +0200

    List shown on browser no functionality.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("Employees")
+            .then((response) => {
+                return response.json()
+            })
+            .then((employees) => {
+                this.setState({ employees: employees })
+            })
+    }
 
   render () {
     return (
-      <div>Complete your app here</div>
+        <div>
+            <h2>Employees Data</h2>
+            <b>Add a new Employee</b><button>New</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Key</th>
+                        <th>Name</th>
+                        <th>Value</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.employees.map((emp, i) => (
+                        <tr key={i}>
+                            <td>{i+1}</td>
+                            {Object.values(emp).map(val => { return <td>{val}</td> })}
+                            <td><button>Delete</button><button>Edit</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
   }
 }
